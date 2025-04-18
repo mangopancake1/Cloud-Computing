7fde6dd18652b7b1ac16a2b12391c57e2c57545b
mangopancake1 <arrafinuristiawan445@gmail.com>
2025-04-19 01:55:30 +0700

    done

--- frontend/src/components/AddPost.js
+++ frontend/src/components/AddPost.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils";
 
 const AddPost = () => {
   const [title, setTitle] = useState("");
