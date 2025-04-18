0a4f573f43bd32bd28fd9c05a253392186ba6339
mangopancake1 <arrafinuristiawan445@gmail.com>
2025-04-19 01:59:14 +0700

    done fix

--- frontend/src/components/EditPost.js
+++ frontend/src/components/EditPost.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import { BASE_URL } from "../utils";
+import { BASE_URL } from "../api";
 
 const EditPost = () => {
   const [title, setTitle] = useState("");
--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { BASE_URL } from "../utils";
+import { BASE_URL } from "../api";
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
