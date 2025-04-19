e004f67d029bff3480d56d13f331911346a53b62
mangopancake1 <arrafinuristiawan445@gmail.com>
2025-04-19 15:18:35 +0700

    update

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('crud','root','', {
+const db = new Sequelize('RECOVER_YOUR_DATA','root','', {
     host: '34.67.189.246',
     dialect: 'mysql',
     logging: false, 
