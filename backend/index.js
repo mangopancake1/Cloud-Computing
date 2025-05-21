b3b6e93e8bcaa2b2c7720c556ab7f67b0d0beab9
mangopancake1 <arrafinuristiawan445@gmail.com>
2025-05-21 15:15:39 +0700

    auth update

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT;
 
 // ✅ Konfigurasi CORS
 const allowedOrigins = [
