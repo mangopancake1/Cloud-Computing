edc47eaf1c29d4ccb0075873f59b270bc9ebf8a6
mangopancake1 <arrafinuristiawan445@gmail.com>
2025-05-24 00:26:35 +0700

    pls

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,17 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  console.log("Authorization Header:", req.headers["authorization"]);
-  const token = authHeader && authHeader.split(" ")[1];
-  console.log("masuk verify token: ", { token });
-
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
   if (!token) return res.sendStatus(401);
-  console.log("sudah lewat 401 di verify");
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
-    console.log("sudah lewat 403 di verify");
-    req.email = decoded.email;
+    req.user = user;
     next();
   });
 };
