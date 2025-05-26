86136a4f653eddfbb9b6bb4caa6d9477602e62b0
mangopancake1 <arrafinuristiawan445@gmail.com>
2025-05-26 17:27:52 +0700

    update

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5005", // atau URL backend kamu yang valid
+  baseURL: "https://notes-backend141-637581838712.us-central1.run.app", // atau URL backend kamu yang valid
   withCredentials: true,
 });
 
